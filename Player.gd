85187b3f0319740495482175049e2f0050525a7d
Etoki <baksyanya2006@gmail.com>
2024-05-27 03:01:14 +0300

    игрок теперь может кушать мобов

--- Player.gd
+++ Player.gd
@@ -12,11 +12,8 @@ func _ready():
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
-		# надо развернуть вправо
-		
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
-		#надо развернуть влево
 		velocity.x -= 1
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
@@ -38,14 +35,10 @@ func _process(delta):
 		# See the note below about boolean assignment.
 		$AnimatedSprite2D.flip_h = velocity.x > 0
 
-
-func _on_body_entered(body):
-	hide() # Player disappears after being hit.
-	hit.emit()
-	# Must be deferred as we can't change physics properties on a physics callback.
-	$CollisionShape2D.set_deferred("disabled", true)
-	
 func start(pos):
 	position = pos
 	show()
 	$CollisionShape2D.disabled = false
+
+func _on_body_entered(body):
+	body.queue_free()
